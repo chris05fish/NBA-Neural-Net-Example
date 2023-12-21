c465ba1035c965cd5e2a8ac96763e2b6b60d2139
Jef Fisher <jeffisher@Jefs-Air.attlocal.net>
2023-12-20 17:12:09 -0800

    Added testing and accuracy

--- nn.py
+++ nn.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from nba_api.stats.endpoints import leaguegamefinder
 
 # Function to fetch NBA game data from the API
@@ -11,7 +12,7 @@ def fetch_nba_data(season):
     game_finder = leaguegamefinder.LeagueGameFinder(season_nullable=season)
     games = game_finder.get_data_frames()[0]
     # Extract relevant features and labels from the retrieved data
-    features = games[['TEAM_ID', 'GAME_DATE', 'PTS', 'AST', 'REB']]  # Modify based on your needs
+    features = games[['TEAM_ID', 'GAME_DATE', 'PTS', 'AST', 'REB']]  
     labels = games['WL'].apply(lambda x: 1 if x == 'W' else 0)  # Convert 'W'/'L' to binary labels
     return features, labels
 
@@ -19,7 +20,7 @@ def fetch_nba_data(season):
 season = '2021-22'
 features, labels = fetch_nba_data(season)
 
-# Create your custom dataset
+# Create custom dataset
 class NBADataset(torch.utils.data.Dataset):
     def __init__(self, features, labels):
         numeric_features = features[['PTS', 'AST', 'REB']]  # Adjust based on your selected features
@@ -32,8 +33,23 @@ class NBADataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         return self.features[index], self.labels[index]
 
-nba_dataset = NBADataset(features, labels)
-nba_dataloader = DataLoader(nba_dataset, batch_size=10, shuffle=True)
+# Split the dataset into training and testing sets
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.2, random_state=42
+)
+
+# Create DataLoader for the training set
+train_dataset = NBADataset(features_train, labels_train)
+train_dataloader = DataLoader(train_dataset, batch_size=10, shuffle=True)
+
+# Create DataLoader for the testing set
+test_dataset = NBADataset(features_test, labels_test)
+test_dataloader = DataLoader(test_dataset, batch_size=10, shuffle=False)
+
+# Define your model, optimizer, and loss function
+input_size = len(features.columns) - 2
+hidden_size = 128  
+output_size = 2  # Binary classification: win or lose
 
 class NBAWinPredictor(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -48,22 +64,34 @@ class NBAWinPredictor(nn.Module):
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 
-# Assuming you have features with appropriate dimensions for your NBA dataset
-input_size = len(features.columns) - 2
-hidden_size = 64  # Adjust as needed
-output_size = 2  # Binary classification: win or lose
-
 nba_model = NBAWinPredictor(input_size, hidden_size, output_size)
 optimizer = optim.Adam(nba_model.parameters(), lr=0.001)
 
-# Training loop (modify as needed)
-EPOCHS = 3
+# Training loop 
+EPOCHS = 10
 for epoch in range(EPOCHS):
-    for data in nba_dataloader:
+    nba_model.train()  # Set the model to training mode
+    for data in train_dataloader:
         features, labels = data
         optimizer.zero_grad()
-        output = nba_model(features.float())  # Ensure features are converted to float
+        output = nba_model(features.float())
         loss = F.nll_loss(output, labels)
         loss.backward()
         optimizer.step()
-    print(loss)
+
+    # Evaluate the model on the testing set
+    nba_model.eval()  # Set the model to evaluation mode
+    all_predictions = []
+    all_labels = []
+
+    with torch.no_grad():
+        for data in test_dataloader:
+            features, labels = data
+            output = nba_model(features.float())
+            _, predictions = torch.max(output, 1)
+            all_predictions.extend(predictions.numpy())
+            all_labels.extend(labels.numpy())
+
+    # Calculate accuracy on the testing set
+    accuracy = accuracy_score(all_labels, all_predictions)
+    print(f"Epoch {epoch + 1}/{EPOCHS}, Loss: {loss.item()}, Accuracy: {accuracy}")
